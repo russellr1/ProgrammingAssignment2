f14dd158bf9a8d69661ffaa7e66530a498e32769
russellr1 <russell.mate@gmail.com>
2014-09-21 17:01:23 +0530

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -2,7 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## The function makeCacheMatrix has four functions:
+## 1. setmatrix, sets/caches the value of the matrix
+## 2. getmatrix, gets the value of the matrix, if it already exists
+## 3. setinv, sets/caches the value of the inv
+## 4. getin, gets the value of the pre-calculated inverse
 makeCacheMatrix <- function(x = matrix())
 {	
 	m <- NULL
@@ -19,18 +23,19 @@ makeCacheMatrix <- function(x = matrix())
 
 
 ## Write a short comment describing this function
-
+## The function cacheSolve computes the inverse of the matrix returned by makeCacheMatrix
+## If the inverse is pre-calculated then retrieves it from the cache.
 cacheSolve <- function(x, ...)
 {
-  ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
 	m <- x$getinv()
 	if(!is.null(m))
 	{
-		message("getting cached data")
+		message("getting cached inverse")
 		return(m)
 	}
 	data <- x$getmatrix()
-	m <- solve(data, ...)
+	m <- solve(data)
 	x$setmatrix(m)
 	m
 }
